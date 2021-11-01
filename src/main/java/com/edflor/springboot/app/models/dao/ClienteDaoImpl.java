f9539d42a320d2ce50d150f4b2ec29ab942ae529
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-01 13:06:54 -0600

    Implementacion de la interface CrudRepository

--- src/main/java/com/edflor/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/edflor/springboot/app/models/dao/ClienteDaoImpl.java
@@ -1,6 +1,6 @@
 package com.edflor.springboot.app.models.dao;
 
-import java.util.List;
+/*import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,48 +8,37 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.edflor.springboot.app.models.entity.Cliente;
+import com.edflor.springboot.app.models.entity.Cliente;*/
 
-@Repository("clienteDaoJPA")
-public class ClienteDaoImpl implements IClienteDao {
+//@Repository("clienteDaoJPA")
+public class ClienteDaoImpl {
+//implements IClienteDao {
 
-	@PersistenceContext
-	private EntityManager em;
-
-	@SuppressWarnings("unchecked")
-	//@Transactional(readOnly = true)
-	@Override
-	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
-		return em.createQuery("from Cliente").getResultList();
-	}
-
-	@Override
-	//@Transactional
-	public void save(Cliente cliente) {
-		if (cliente.getId() != null && cliente.getId() > 0) {
-			em.merge(cliente);
-		} else {
-			em.persist(cliente);
-		}
-
-	}
-
-	@Override
-	//@Transactional(readOnly = true)
-	public Cliente findOne(Long id) {
-		// TODO Auto-generated method stub
-		return em.find(Cliente.class, id);
-	}
-
-	@Override
-	//@Transactional
-	public void delete(Long id) {
-		/*
-		 * Cliente cliente = findOne(id); em.remove(cliente);
-		 */
-		
-		em.remove(findOne(id));
-	}
+	/*
+	 * @PersistenceContext private EntityManager em;
+	 * 
+	 * @SuppressWarnings("unchecked") //@Transactional(readOnly = true)
+	 * 
+	 * @Override public List<Cliente> findAll() { // TODO Auto-generated method stub
+	 * return em.createQuery("from Cliente").getResultList(); }
+	 * 
+	 * @Override //@Transactional public void save(Cliente cliente) { if
+	 * (cliente.getId() != null && cliente.getId() > 0) { em.merge(cliente); } else
+	 * { em.persist(cliente); }
+	 * 
+	 * }
+	 * 
+	 * @Override //@Transactional(readOnly = true) public Cliente findOne(Long id) {
+	 * // TODO Auto-generated method stub return em.find(Cliente.class, id); }
+	 * 
+	 * @Override
+	 * 
+	 * @Transactional public void delete(Long id) {
+	 * 
+	 * Cliente cliente = findOne(id); em.remove(cliente);
+	 * 
+	 * 
+	 * em.remove(findOne(id)); }
+	 */
 
 }
--- src/main/java/com/edflor/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/edflor/springboot/app/models/dao/IClienteDao.java
@@ -1,17 +1,21 @@
 package com.edflor.springboot.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+//import java.util.List;
 
 import com.edflor.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
 
-	public List<Cliente> findAll();
-	
-	public void save(Cliente cliente);
-	
-	public Cliente findOne(Long id);
-	
-	public void delete(Long id);
+	/*
+	 * public List<Cliente> findAll();
+	 * 
+	 * public void save(Cliente cliente);
+	 * 
+	 * public Cliente findOne(Long id);
+	 * 
+	 * public void delete(Long id);
+	 */
 	
 }
