1bdc9f6e5610ab605e929ad7b1f71953c1c19063
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-04 13:05:19 -0600

    clase ServiceImpl

--- src/main/java/com/edflor/springboot/app/models/service/UploadServiceImpl.java
+++ src/main/java/com/edflor/springboot/app/models/service/UploadServiceImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -70,5 +71,19 @@ public class UploadServiceImpl implements IUploadFileService {
 	public Path getPath(String filename) {
 		return Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
 
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		FileSystemUtils.deleteRecursively(Paths.get(UPLOADS_FOLDER).toFile());
+		
+	}
+
+	@Override
+	public void init() throws IOException {
+		// TODO Auto-generated method stub
+		Files.createDirectories(Paths.get(UPLOADS_FOLDER));
+		
 	}
 }
